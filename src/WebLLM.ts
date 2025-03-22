ce0e85830db93f4c5631cccb09cc166b99f12423
Giuseppe Toto <billyball85@gmail.com>
2025-03-22 15:35:16 +0100

    enable sourcemap + minor fixes

--- src/WebLLM.ts
+++ src/WebLLM.ts
@@ -2,53 +2,45 @@ import {
     CreateExtensionServiceWorkerMLCEngine,
     MLCEngineInterface,
     InitProgressReport,
-    ChatCompletionMessageParam,
-    ResponseFormat
-  } from "@mlc-ai/web-llm";
+    ChatCompletionRequestStreaming
+} from "@mlc-ai/web-llm";
 
 export default class WebLLM {
 
-    private engine: MLCEngineInterface;
+    private engine!: MLCEngineInterface;
 
-    public static createAsync(initProgressCallback,model:string = 'Llama-3.1-8B-Instruct-q4f16_1-MLC'): WebLLM{
-       const instance = new WebLLM();
-       instance.setup(model,initProgressCallback);
-       console.log("WEBLLM INITIALIZED");
-       return instance;
-    }
-    async setup(model,initProgressCallback) {
-         this.engine= await CreateExtensionServiceWorkerMLCEngine(
-            model,
-            { initProgressCallback: initProgressCallback },
-          );
+    public static async createAsync(initProgressCallback: (report: InitProgressReport) => void, model: string = 'Llama-3.1-8B-Instruct-q4f16_1-MLC'): Promise<WebLLM> {
+        const instance = new WebLLM();
+        await instance.setup(model, initProgressCallback);
+        console.log("WEBLLM INITIALIZED");
+        return instance;
     }
 
-    public async sendMessage(message: string, callback) {
-        const messages: ChatCompletionMessageParam[] = [];
-      
-        messages.push({ role: "user", content: message });
-      
-         let curMessage = "";
-         const completion = await this.engine.chat.completions.create({
-           stream: true,
-           messages: messages,
-           response_format: { type: 'text' },
-         });
-         console.log("sending message...");
-       
-         // Update the answer as the model generates more text
-         for await (const chunk of completion) {
-      
-           const curDelta = chunk.choices[0].delta.content;
-           if (curDelta) {
-             curMessage += curDelta;
-           }
-           callback(curMessage);
-         }
-         message= await this.engine.getMessage();
-         messages.push({ role: "assistant", content: await this.engine.getMessage() });
-      }
-      
+    private async setup(model: string, initProgressCallback: (report: InitProgressReport) => void): Promise<void> {
+        this.engine = await CreateExtensionServiceWorkerMLCEngine(model, {initProgressCallback: initProgressCallback},);
+    }
+
+    public async sendMessage(message: string, onResponseUpdate: (accumulatedResponse: string) => void) {
+
+        const options:ChatCompletionRequestStreaming={
+            stream: true,
+            messages: [{role: "user", content: message}],
+            response_format: {type: 'text'},
+        }
+        const streamingCompletion = await this.engine.chat.completions.create(options);
+        console.log("sending message...");
+
+        // Update the answer as the model generates more text
+        let accumulatedResponse = "";
+        for await (const chunk of streamingCompletion) {
+            const responseFragment = chunk.choices[0].delta.content;
+            if (responseFragment) {
+                accumulatedResponse += responseFragment;
+            }
+            onResponseUpdate(accumulatedResponse);
+        }
+
+    }
 
 
 }
